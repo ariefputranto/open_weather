dec5c78bce474d592b964bec041521d995cf5b27
Arief Putranto <arif.putranto@smartcloud.id>
2018-12-14 01:45:43 +0700

    add datatable and google map

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -40,14 +40,6 @@ class DefaultController extends Controller
     		->getQuery();
     	$result = $query->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
 
-    	// $result = [];
-    	// foreach ($tmp as $key => $val) {
-    	// 	$result[$key] = json_encode([
-    	// 		'id' => $val['postcode'],
-    	// 		'text' => $val['placeName']
-    	// 	]);
-    	// }
-
     	$response = new Response();
 		$response->setContent(json_encode($result));
 		$response->headers->set('Content-Type', 'application/json');
@@ -60,13 +52,54 @@ class DefaultController extends Controller
      *
      * @Route("/map", name="default_map")
      */
-    public function map(Request $request)
+    public function map()
     {
-    	var_dump ($request);
-    	die();
-        $number = random_int(0, 100);
-        $result = ApiController::open_weather('kediri');
-        var_dump (json_decode($result));
-        die();
+    	$request = Request::createFromGlobals();
+    	$location = $request->query->get('location');
+    	$radius = $request->query->get('radius');
+
+    	$city = $this->getDoctrine()->getRepository(City::class);
+    	$query = $city->createQueryBuilder('c')
+    		->where('c.id != :id')
+    		->setParameter('id', $location)
+    		->orderBy('c.placeName')
+    		->getQuery();
+    	$result_all = $query->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+
+    	$query = $city->createQueryBuilder('c')
+    		->where('c.id = :id')
+    		->setParameter('id', $location)
+    		->orderBy('c.placeName')
+    		->setMaxResults(1)
+    		->getQuery();
+    	$result_current_location = $query->getOneOrNullResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+
+    	$marker = [];
+    	foreach ($result_all as $key => $val) {
+    		$distance = self::haversineGreatCircleDistance($val['latitude'],$val['longitude'],$result_current_location['latitude'], $result_current_location['longitude']);
+    		if ($distance <= $radius) {
+    			$marker []= $val;
+    		}
+    	}
+
+    	return $this->render('map.html.twig', array(
+    		'marker' => $marker,
+   			'current_loc' => $result_current_location
+    	));
     }
+
+    private function haversineGreatCircleDistance($latitude_from, $longitude_from, $latitude_to, $longitude_to, $earth_radius = 6371)
+	{
+	  $lat_from = deg2rad($latitude_from);
+	  $long_from = deg2rad($longitude_from);
+	  $lat_to = deg2rad($latitude_to);
+	  $long_to = deg2rad($longitude_to);
+
+	  $lat_delta = $lat_to - $lat_from;
+	  $long_delta = $long_to - $long_from;
+
+	  $angle = 2 * asin(sqrt(pow(sin($lat_delta / 2), 2) +
+	    cos($lat_from) * cos($lat_to) * pow(sin($long_delta / 2), 2)));
+	  return $angle * $earth_radius;
+	}
 }
